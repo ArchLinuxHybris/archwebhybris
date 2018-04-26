5eb4ae1264ddbbb277c13d04b0db56341e940908
Erich Eckner <git@eckner.net>
2018-04-26 15:47:04 +0200

    lib/style.php: new to reduce duplicate code

--- packages/index.php
+++ packages/index.php
@@ -1,6 +1,7 @@
 <?php
 
   include "lib/mysql.php";
+  include "lib/style.php";
 
   foreach (array("bugs","sort") as $expected_param)
     if (! isset($_GET[$expected_param]))
@@ -276,34 +277,9 @@
 
   };
 
+  print_header("Package Search");
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <title>Arch Linux 32 - Package Search</title>
-    <link rel="stylesheet" type="text/css" href="/static/archweb.css" media="screen, projection" />
-    <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
-    <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
-  </head>
-  <body class="">
-<?php show_warning_on_offline_slave(); ?>
-    <div id="archnavbar" class="anb-packages">
-      <div id="archnavbarlogo"><h1><a href="/" title="Return to the main page">Arch Linux</a></h1></div>
-      <div id="archnavbarmenu">
-        <ul id="archnavbarlist">
-          <li id="anb-home"><a href="https://www.archlinux32.org/">Home</a></li>
-          <li id="anb-news"><a href="https://news.archlinux32.org/">News</a></li>
-          <li id="anb-packages"><a href="https://packages.archlinux32.org/">Packages</a></li>
-          <li id="anb-forums"><a href="https://bbs.archlinux32.org/">Forums</a></li>
-          <li id="anb-bugs"><a href="https://bugs.archlinux32.org/" title="Report and track bugs">Bugs</a></li>
-          <li id="anb-mailing-list"><a href="https://lists.archlinux.org/listinfo/arch-ports">Mailing List</a></li>
-          <li id="anb-download"><a href="https://www.archlinux32.org/download/" title="Get Arch Linux">Download</a></li>
-          <li id="anb-arch-linux-official"><a href="https://www.archlinux.org/">Arch Linux Official</a></li>
-        </ul>
-      </div>
-    </div>
-    <div id="content">
       <div id="pkglist-search" class="box filter-criteria">
         <h2>Package Search</h2>
         <form id="pkg-search" method="get" action="/">
@@ -442,29 +418,6 @@ if (count($exact_matches) > 0) {
           detailed information about packages located in the 32 bit repositories.
         </p>
       </div>
-      <div id="footer">
-        <p>
-          Copyright © 2002-2018 <a href="mailto:jvinet@zeroflux.org" title="Contact Judd Vinet">Judd Vinet</a> and <a href="mailto:aaron@archlinux.org" title="Contact Aaron Griffin">Aaron Griffin</a>.
-        </p>
-        <p>
-          The Arch Linux name and logo are recognized <a href="https://wiki.archlinux.org/index.php/DeveloperWiki:TrademarkPolicy" title="Arch Linux Trademark Policy">trademarks</a>. Some rights reserved.
-        </p>
-        <p>
-          The registered trademark Linux® is used pursuant to a sublicense from LMI, the exclusive licensee of Linus Torvalds, owner of the mark on a world-wide basis.
-        </p>
-      </div>
-    </div>
-    <script type="application/ld+json">
-    {
-      "@context": "http://schema.org",
-      "@type": "WebSite",
-      "url": "/",
-      "potentialAction": {
-        "@type": "SearchAction",
-        "target": "/?q={search_term}",
-        "query-input": "required name=search_term"
-      }
-    }
-    </script>
-  </body>
-</html>
+<?php
+
+  print_footer();
--- packages/pkginfo.php
+++ packages/pkginfo.php
@@ -1,6 +1,7 @@
 <?php
 
   include "lib/mysql.php";
+  include "lib/style.php";
 
   $json_content = json_decode(
     file_get_contents(
@@ -228,36 +229,9 @@
   while ($row = $mysql_result -> fetch_assoc())
     $elsewhere[] = $row;
 
+  print_header($content["Name"] . " " . $content["Version"] . " (" . $content["Architecture"] . ")");
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <title>Arch Linux 32 - <?php print $content["Name"] . " " . $content["Version"] . " (" . $content["Architecture"]; ?>)</title>
-    <link rel="stylesheet" type="text/css" href="/static/archweb.css" media="screen, projection" />
-    <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
-    <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
-  </head>
-  <body class="">
-<?php show_warning_on_offline_slave(); ?>
-    <div id="archnavbar" class="anb-packages">
-      <div id="archnavbarlogo">
-        <h1><a href="/" title="Return to the main page">Arch Linux</a></h1>
-      </div>
-      <div id="archnavbarmenu">
-        <ul id="archnavbarlist">
-          <li id="anb-home"><a href="https://www.archlinux32.org/">Home</a></li>
-          <li id="anb-news"><a href="https://news.archlinux32.org/">News</a></li>
-          <li id="anb-packages"><a href="https://packages.archlinux32.org/">Packages</a></li>
-          <li id="anb-forums"><a href="https://bbs.archlinux32.org/">Forums</a></li>
-          <li id="anb-bugs"><a href="https://bugs.archlinux32.org/" title="Report and track bugs">Bugs</a></li>
-          <li id="anb-mailing-list"><a href="https://lists.archlinux.org/listinfo/arch-ports">Mailing List</a></li>
-          <li id="anb-download"><a href="https://www.archlinux32.org/download/" title="Get Arch Linux">Download</a></li>
-          <li id="anb-arch-linux-official"><a href="https://www.archlinux.org/">Arch Linux Official</a></li>
-        </ul>
-      </div>
-    </div>
-    <div id="content">
       <div id="archdev-navbar">
       </div>
       <div id="pkgdetails" class="box">
@@ -531,29 +505,6 @@ if (count($elsewhere)>0) {
           </div>
         </div>
       </div>
-      <div id="footer">
-        <p>
-          Copyright © 2002-2018 <a href="mailto:jvinet@zeroflux.org" title="Contact Judd Vinet">Judd Vinet</a> and <a href="mailto:aaron@archlinux.org" title="Contact Aaron Griffin">Aaron Griffin</a>.
-        </p>
-        <p>
-          The Arch Linux name and logo are recognized <a href="https://wiki.archlinux.org/index.php/DeveloperWiki:TrademarkPolicy" title="Arch Linux Trademark Policy">trademarks</a>. Some rights reserved.
-        </p>
-        <p>
-          The registered trademark Linux® is used pursuant to a sublicense from LMI, the exclusive licensee of Linus Torvalds, owner of the mark on a world-wide basis.
-        </p>
-      </div>
-    </div>
-    <script type="application/ld+json">
-      {
-        "@context": "http://schema.org",
-        "@type": "WebSite",
-        "url": "/",
-        "potentialAction": {
-          "@type": "SearchAction",
-          "target": "/?q={search_term}",
-          "query-input": "required name=search_term"
-        }
-      }
-    </script>
-  </body>
-</html>
+<?php
+
+  print_footer();
