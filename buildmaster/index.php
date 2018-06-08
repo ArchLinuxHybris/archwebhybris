a476360e016e7f88dee9429b463c57a2aba122df
Erich Eckner <git@eckner.net>
2018-06-08 15:36:43 +0200

    buildmaster/index.php: add more links

--- buildmaster/index.php
+++ buildmaster/index.php
@@ -11,13 +11,15 @@
 <?php show_warning_on_offline_slave(); ?>
     <a href="/buildmaster/build-list.php">build list</a>
     <a href="/buildmaster/build-list.php?show=broken">broken packages</a><br>
-    <a href="/buildmaster/build-slaves.php">build-slaves</a><br>
-    <a href="/buildmaster/status.php">status</a>
+    <a href="/buildmaster/build-slaves.php">build-slaves</a>
+    <a href="/buildmaster/status.php">status</a><br>
+    <a href="https://buildmaster.archlinux32.org/build-logs/">build logs</a><br>
     <a href="/buildmaster/log.php?show=ssh">ssh-log</a>
     <a href="/buildmaster/log.php?show=email">email-log</a><br>
     <a href="/buildmaster/mysql-issues.php">broken dependencies in the database</a><br>
     <a href="/buildmaster/todos.php">todos</a>
     as <a href="/buildmaster/todos.php?graph">graph</a><br>
+    <a href="https://buildmaster.archlinux32.org/database-layout.png">database layout</a><br>
     <a href="/buildmaster/blacklist.php">blacklisted packages</a>,
     <a href="/buildmaster/to-delete.php">packages to be deleted</a>
     and <a href="/buildmaster/deletion-links.php">links between them</a><br>
