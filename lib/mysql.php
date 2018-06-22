857d7d193c454ff18c4c15089bc465178415a5a8
Erich Eckner <git@eckner.net>
2018-06-22 13:32:13 +0200

    lib/mysql.php: do not print warning if last ssh_log is less than a minute ago

--- lib/mysql.php
+++ lib/mysql.php
@@ -20,9 +20,18 @@ function mysql_run_query($query) {
 }
 
 function show_warning_on_offline_slave() {
-  $result = mysql_run_query("SHOW STATUS LIKE \"Slave_running\"");
+  $result = mysql_run_query(
+    "SHOW STATUS LIKE \"Slave_running\""
+  );
   if (($result -> num_rows == 0) ||
     ($result -> fetch_assoc() ["Value"] != "ON")) {
-    print "<div><font color=\"ff0000\">The replication slave is currently not running. The database might be outdated.</font></div>\n";
+    $result = mysql_run_query(
+      "SELECT 1 FROM `ssh_log`" .
+      " WHERE ADDTIME(`ssh_log`.`date`,\"1:00\")>NOW()" .
+      " LIMIT 1"
+    );
+    if ($result -> num_rows == 0) {
+      print "<div><font color=\"ff0000\">The replication slave is currently not running. The database might be outdated.</font></div>\n";
+    }
   }
 }
